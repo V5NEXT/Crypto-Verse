d728325699160389cb64b8efd129c3a6ec3a377b
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2023-01-04 18:45:05 +0100

    Fixed Route Issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import react from "react";
-import  ReactDOM  from "react-dom";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
+    <App />
+    </Router>
+);
+
+
