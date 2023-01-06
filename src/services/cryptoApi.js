9168196efb5bb3c8241424ae0f0ea1d678357fb3
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2023-01-06 15:39:03 +0100

    Added service section

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,27 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '7120a36c3bmshe6191fdfe36e028p16a784jsne37840eef29b',
-// 		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-// 	}
-// };
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '7120a36c3bmshe6191fdfe36e028p16a784jsne37840eef29b',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url)=> ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath : 'cryptoApi',
+    baseQuery : fetchBaseQuery({baseUrl}),
+    endpoints : (builder) =>({
+        getCryptos: builder.query({
+            getCryptos: builder.query({
+                query: () => createRequest('/coins')
+            })
+        })
+    })
+
+})
+
+export const{
+    useGetCryptosQuery,
+} = cryptoApi;
