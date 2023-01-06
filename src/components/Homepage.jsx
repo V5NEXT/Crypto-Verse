72783afed8750628a119f636432b7d71100b8676
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2023-01-06 15:04:06 +0100

    Added store

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
+const {Title} = Typography;
+ 
 const Homepage = () => {
   return (
-    <div className='container'>
-      <div>
+    <>
+      <Title level={2} className='heading'></Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Crytpocurrencies" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={5}/></Col>
+        <Col span={12}><Statistic title="Total 24h Voloume" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Markets" value={5}/></Col>
+      </Row>
+    </>
 
-      </div>
-    </div>
   )
 }
 
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,7 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '7120a36c3bmshe6191fdfe36e028p16a784jsne37840eef29b',
+// 		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+// 	}
+// };
